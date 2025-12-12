8ecc38aa72b51ed5021f0bffa35527f7f91222c0
Nicholas Rios <nicholas.jay.rios@gmail.com>
2025-12-12 00:28:21 -0600

    ok relz

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,11 @@
-// tailwind.config.js (content)
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './contexts/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
